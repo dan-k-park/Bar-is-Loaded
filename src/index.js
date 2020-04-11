01140d0d028ac2eead6671226ce0de2ffe4d1d67
Daniel Park <parkdk96@gmail.com>
2020-04-11 01:25:19 -1000

    initial canvas setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+  let canvas = document.getElementById("visualizer")
+  let ctx = canvas.getContext("2d")
+  let input = document.getElementById("weight")
+
+
+  createBar(ctx)
 })
+
+
+
+const createBar = ctx => {
+  ctx.fillRect(0, 170, 40, 180)
+}
