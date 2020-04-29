ac936bcba34f9c7c47e7c90e8903b781b1ebb8af
Daniel Park <parkdk96@gmail.com>
2020-04-28 21:49:56 -1000

    working on creating bar

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let canvas = document.getElementById("visualizer")
-  let ctx = canvas.getContext("2d")
+let WEIGHTS = [
+  {w: 25, color: 'red'}
+]
 
-  createBar(ctx)
+document.addEventListener('DOMContentLoaded', () => {
+  let canvas = document.getElementById('visualizer')
+  let ctx = canvas.getContext('2d')
+
+  createBar(ctx, canvas)
 })
 
 
 
-const createBar = ctx => {
-  ctx.fillRect(0, 0, 40, 180)
-  let input = document.getElementById("weight")
+const createBar = (ctx, canvas) => {
+  console.log(canvas.width)
+  ctx.beginPath()
+  ctx.rect(canvas.width - 30, canvas.height / 2 - 10, 30, 20)
+  ctx.fillStyle = '#cccccc'
+  ctx.fill();
+
+  let input = document.getElementById('weight')
   input.addEventListener('input', createWeights)
 }
 
 const createWeights = evt => {
+  // if (evt.target.value < 25) {
+  //   alert('Must be more than 25kg')
+  // }
   console.log(evt.target.value)
 }
